4dfef53def41c9eaae4a24c8b1bab3417ad4a643
nima2025ne-glitch <nima2025ne@gmail.com>
2025-11-05 23:56:21 +0330

    Update vite.config.js

--- client/my-react-app/vite.config.js
+++ client/my-react-app/vite.config.js
@@ -3,9 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: './', // ✅ مسیرهای CSS/JS نسبی میشن و روی Render درست لود میشه
   server: {
     port: 5173,
     proxy: {
+      // 🔹 فقط برای dev، localhost درست میمونه
       '/api': 'http://localhost:8000',
     },
   },
